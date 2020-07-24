08af740fcfefd4cbb90ff916777f46d2419b8b84
John Dela Cruz <kenneth1521412@gmail.com>
2020-07-24 09:25:31 +0100

    done july 24th

---
+++ Program/Repeat.py
@@ -0,0 +1,19 @@
+def times(input,num):
+    word = ""
+    total = 0
+    for i in range(input):
+        word = word + num
+        total = total + int(word)
+        
+        
+
+    
+
+    return total
+
+
+
+
+
+
+print(times(4,"9"))
